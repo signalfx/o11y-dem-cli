035156207ff05f255f3d731443c3f017313f3766
Max Virgil <mvirgil@splunk.com>
2024-10-22 21:13:35 -0700

    Add eslint header plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
+// ESLint v9 work-around: https://github.com/Stuk/eslint-plugin-header/issues/57
+const pluginHeader = require('eslint-plugin-header');
+pluginHeader.rules.header.meta.schema = false;
+
 module.exports = {
   extends: [
     'eslint:recommended',
@@ -7,6 +11,28 @@ module.exports = {
   parserOptions: {
     project: true
   },
-  plugins: ['@typescript-eslint'],
+  plugins: [
+    '@typescript-eslint',
+    'header',
+  ],
   root: true,
+  rules: {
+    'header/header': [2, 'block', [
+      '',
+      ' * Copyright Splunk Inc.',
+      ' *',
+      ' * Licensed under the Apache License, Version 2.0 (the "License");',
+      ' * you may not use this file except in compliance with the License.',
+      ' * You may obtain a copy of the License at',
+      ' *',
+      ' *     http://www.apache.org/licenses/LICENSE-2.0',
+      ' *',
+      ' * Unless required by applicable law or agreed to in writing, software',
+      ' * distributed under the License is distributed on an "AS IS" BASIS,',
+      ' * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.',
+      ' * See the License for the specific language governing permissions and',
+      ' * limitations under the License.',
+      ''
+    ], 2]
+  }
 };
