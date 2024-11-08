227036739966837a331d66fa9386d282aa2788f9
Tony <tonzhan2@cisco.com>
2024-11-08 13:55:36 -0800

    updated enoent tests

--- test/utils/httpUtils.test.ts
+++ test/utils/httpUtils.test.ts
@@ -72,7 +72,7 @@ describe('uploadFile', () => {
       fail('Expected error was not thrown');
     } catch (e) {
       if (e instanceof Error) {
-        notStrictEqual((e as Error).message, 'ENOENT ....');
+        equal((e as NodeJS.ErrnoException).code, 'ENOENT');
       } else {
         fail('Caught error is not an instance of Error');
       }
@@ -112,7 +112,7 @@ describe('uploadFile', () => {
       fail('Expected error was not thrown');
     } catch (e) {
       if (e instanceof Error) {
-        notStrictEqual((e as Error).message, 'ENOENT ....');
+        equal((e as NodeJS.ErrnoException).code, 'ENOENT');
       } else {
         fail('Caught error is not an instance of Error');
       }
