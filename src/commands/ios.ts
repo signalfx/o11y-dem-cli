f14a5f5998bcbb016574be990c18c7c274b87d2b
Carlos McEvilly <cmcevill@cisco.com>
2024-12-23 08:28:15 -0800

    DEMRUM-439: implement iOS dSYMs upload

--- src/commands/ios.ts
+++ src/commands/ios.ts
@@ -15,13 +15,131 @@
 */
 
 import { Command } from 'commander';
+import axios from 'axios';
+import {
+  isValidFile,
+  hasValidExtension,
+  isValidAppId,
+  isValidVersionCode,
+  isValidUUID
+} from '../utils/iOSInputValidations';
+import { UserFriendlyError } from '../utils/userFriendlyErrors';
+import { createLogger, LogLevel } from '../utils/logger';
+import { uploadFile } from '../utils/httpUtils';
 
-export const iosCommand = new Command('ios');
+// Constants
+const DEFAULT_REALM = 'us0';
+const DSYM_FIELD_NAME = 'dSYM';
+const API_BASE_URL = process.env.API_BASE_URL || 'https://api.splunk.com';
+const API_VERSION_STRING = "v1";
+const API_PATH = "dsyms";
 
-iosCommand
+export const iOSCommand = new Command('iOS');
+
+const iOSUploadDescription = `
+This command uploads the provided dSYMs file.
+You need to provide the Application ID and version code of the app, and the path to the zipped dSYMs file.
+Optionally, you can also include a UUID to identify the upload session.
+`;
+
+const listdSYMsDescription = `
+This command retrieves and shows a list of the uploaded dSYM files.
+By default, it will return the last 100 dSYM files uploaded, sorted in reverse chronological order based on the upload timestamp.
+`;
+
+const generateUrl = (appId: string, versionCode: string): string => {
+  const realm = process.env.O11Y_REALM || DEFAULT_REALM;
+  return `${API_BASE_URL}/${realm}/v1/${API_PATH}/${appId}/${versionCode}`;
+};
+
+interface UploadiOSOptions {
+  file: string;
+  appId: string;
+  versionCode: string;
+  uuid?: string;
+  debug?: boolean;
+}
+
+iOSCommand
+  .name('iOS dSYMs tool')
+  .description('A CLI tool for uploading and checking on iOS symbolication files (dSYMs)')
+  .version('0.1.0');
+
+iOSCommand
   .command('upload')
-  .description('Upload an iOS dSYM file')
-  .requiredOption('--file <file>', 'Path to the dSYM file')
-  .action((options) => {
-    console.log(`Uploading iOS dSYM file from: ${options.file}`);
+  .showHelpAfterError(true)
+  .usage('--app-id <value> --version-code <int> --file <path> [--uuid <value>]')
+  .description(iOSUploadDescription)
+  .summary('Uploads the dSYM file with the provided application ID, version code, and optional UUID')
+  .requiredOption('--app-id <value>', 'Application ID')
+  .requiredOption('--version-code <int>', 'Version code')
+  .requiredOption('--file <path>', 'Path to the dSYMs zip file')
+  .option('--uuid <value>', 'Optional UUID for the upload')
+  .option('--debug', 'Enable debug logs')
+  .action(async (options: UploadiOSOptions) => {
+    const logger = createLogger(options.debug ? LogLevel.DEBUG : LogLevel.INFO);
+
+    try {
+      if (!isValidAppId(options.appId)) {
+        throw new UserFriendlyError(null, 'Invalid Application ID. It must be a non-empty string.');
+      }
+
+      if (!isValidVersionCode(options.versionCode)) {
+        throw new UserFriendlyError(null, 'Invalid Version Code. It must be an integer.');
+      }
+
+      if (!isValidFile(options.file)) {
+        throw new UserFriendlyError(null, `Invalid dSYM file path: ${options.file}.`);
+      }
+
+      if (!hasValidExtension(options.file, '.zip')) {
+        throw new UserFriendlyError(null, `dSYM file does not have correct extension: ${options.file}.`);
+      }
+
+      if (options.uuid && !isValidUUID(options.uuid)) {
+        throw new UserFriendlyError(null, 'Error: Invalid UUID. It must be a non-empty string.');
+      }
+
+      const parameters: { [key: string]: string | number } = {
+        appId: options.appId,
+        versionCode: options.versionCode,
+      };
+
+      if (options.uuid) {
+        parameters.uuid = options.uuid;
+      }
+
+      const fileData = {
+        filePath: options.file,
+        fieldName: DSYM_FIELD_NAME,
+      };
+
+      const url = generateUrl(options.appId, options.versionCode);
+
+      logger.info(`url: ${url}`)
+
+      logger.info(`Preparing to upload dSYM file:
+        App ID: ${options.appId}
+        Version Code: ${options.versionCode}
+        File: ${options.file}
+        UUID: ${options.uuid || 'Not provided'}`
+      );
+
+      await uploadFile({
+        url,
+        file: fileData,
+        parameters,
+      });
+
+      logger.info('\nUpload complete!');
+    } catch (error) {
+      if (error instanceof Error) {
+        logger.error('Failed to upload the dSYM file:', error.message);
+        throw error;
+      } else {
+        const errorMessage = `Unexpected error type: ${JSON.stringify(error)}`;
+	logger.error('Failed to upload the dSYM file:', errorMessage);
+	throw new Error(errorMessage);
+      }
+    }
   });
